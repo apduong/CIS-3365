4487222efa3ccd116bdda9de36ef8872e3471bbf
severinkrystyan <krystyanseverin@gmail.com>
2021-04-15 11:46:45 -0400

    Added comments

--- Hallywell CRMS v1/main.py
+++ Hallywell CRMS v1/main.py
@@ -1,3 +1,8 @@
+from PyQt6 import QtCore, QtGui, QtWidgets
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QColor
+from PyQt6.QtWidgets import *
+# todo: import all form classes here
 from loadingscreen import Ui_LoadingScreen
 from mainscreen import Ui_MainScreen
 from NewProductForm import Ui_NewProductForm
@@ -6,15 +11,12 @@ from ProductDetailForm import Ui_ProductDetails
 from ProductRatingDetail import Ui_ProductRatingDetail
 from ProductStatusDetail import Ui_ProductStatusDetail
 from ProductTypeDetail import Ui_ProductTypeDetail
-from Manufacturer import Ui_Manufacturer
-from PyQt6 import QtCore, QtGui, QtWidgets
-from PyQt6.QtCore import Qt
-from PyQt6.QtGui import QColor
-from PyQt6.QtWidgets import *
+
 
 counter = 0
 
 
+# Loading Screen - DO NOT TOUCH
 class LoadingWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -59,30 +61,92 @@ class MainScreen(QMainWindow):
         self.ui.setupUi(self)
         self.ui.openform_button.clicked.connect(self.open_forms)
 
+    # ADDS FORM NAMES TO FORM DROPDOWN LIST
+    # todo: add all forms to this list
+    # IDEA: We can create a text file or csv file and have it read the list of forms there
     def load_forms(self):
-        self.ui.comboBox_form.addItems(["New Product Form", "Product Color Detail"])
+        self.ui.comboBox_form.addItems(["New Product Form", "Product Detail Form", "Product Color Detail",
+                                        "Product Rating Detail", "Product Status Detail"])
 
+    # THIS FUNCTION IS CALLED WHEN THE OPEN FORM BUTTON IS CLICKED
+    # todo: add logic for each form
     def open_forms(self):
+        # Retrieves selected value from drop down list
         selected = self.ui.comboBox_form.currentText()
         if selected == "New Product Form":
             self.open_productform()
+        elif selected == "Product Detail Form":
+            self.open_productdetail()
+        elif selected == "Product Color Detail":
+            self.open_productcolordetail()
+        elif selected == "Product Rating Detail":
+            self.open_productratingdetail()
+        elif selected == "Product Status Detail":
+            self.open_productstatusdetail()
 
+    # ==> PLACE FORM DISPLAY FUNCTIONS BELOW HERE
+    # todo: add functions to open all forms
+    # NEW PRODUCT FORM
     def open_productform(self):
-        self.form = NewProductForm()
+        self.form = NewProductForm()  # This is set to the design class name
+        self.form.show()
+
+    # PRODUCT DETAIL FORM
+    def open_productdetail(self):
+        self.form = ProductDetail()
+        self.form.show()
+
+    # PRODUCT COLOR DETAIL
+    def open_productcolordetail(self):
+        self.form = ProductColorDetail()
+        self.form.show()
+
+    # PRODUCT RATING DETAIL
+    def open_productratingdetail(self):
+        self.form = ProductRatingDetail()
+        self.form.show()
+
+    # PRODUCT STATUS DETAIL
+    def open_productstatusdetail(self):
+        self.form = ProductStatusDetail()
         self.form.show()
 
 
-class ManufacturerForm(QMainWindow):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.ui = Ui_Manufacturer()
-        self.ui.setupUi(self)
-
-
+# ==> PLACE DESIGN CLASSES BELOW HERE
+# REQ: All form functionality will be added here
+# todo: Create classes for all forms
 class NewProductForm(QMainWindow):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.ui = Ui_NewProductForm()
+        self.ui.setupUi(self)
+
+
+class ProductDetail(QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.ui = Ui_ProductDetails()
+        self.ui.setupUi(self)
+
+
+class ProductColorDetail(QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.ui = Ui_ProductColorDetail()
+        self.ui.setupUi(self)
+
+
+class ProductRatingDetail(QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.ui = Ui_ProductRatingDetail()
+        self.ui.setupUi(self)
+
+
+class ProductStatusDetail(QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.ui = Ui_ProductStatusDetail()
         self.ui.setupUi(self)
 
 
